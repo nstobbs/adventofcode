765ad877e9a5b6ced5180a917a5df313b660ebd1
Nathan Stobbs <nstobbs97@gmail.com>
2024-12-01 19:05:42 +0000

    algoritm sort instead of merge sort

--- src/day1/day1.cpp
+++ src/day1/day1.cpp
@@ -3,67 +3,11 @@
 #include <vector>
 #include <string>
 #include <stdlib.h>
+#include <algorithm>
 
 std::vector<int> input;
 std::vector<int> listOne;
 std::vector<int> listTwo;
-
-// Merge Sort From https://www.geeksforgeeks.org/merge-sort/
-void merge(std::vector<int> &array, int left, int mid, int right)
-{
-    int number1 = mid - left + 1;
-    int number2 = right - mid;
-
-    std::vector<int> L(number1), R(number2);
-    for (int i = 0; i < number1; i++)
-    {
-        L[i] = array[left + i];
-    }
-    for (int j = 0; j < number2; j++)
-    {
-        R[j] = array[mid + 1 + j];
-    }
-
-    int i = 0, j = 0;
-    int k = left;
-
-    while (i < number1 && j < number2)
-    {
-        if(L[i] <= R[j])
-        {
-            array[k] = L[i];
-            i++;
-        } else {
-            array[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < number1)
-    {
-        array[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < number2)
-    {
-        array[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void mergeSort(std::vector<int> &array, int left, int right)
-{
-    if (left >= right)
-        return;
-    int mid = left + (right - left) / 2;
-    mergeSort(array, left, mid);
-    mergeSort(array, mid + 1, right);
-    merge(array, left, mid, right);
-}
 
 int main()
 {
@@ -88,8 +32,8 @@ int main()
         }
     }
     // Sort both list of Array
-    mergeSort(listOne, 0, int(listOne.size())-1);
-    mergeSort(listTwo, 0, int(listTwo.size())-1);
+    std::sort(listOne.begin(), listOne.end());
+    std::sort(listTwo.begin(), listTwo.end());
 
     // Then go throught each item in the list and take away each pair from
     // Add the final result into result var
